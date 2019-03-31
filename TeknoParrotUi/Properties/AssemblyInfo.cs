a546ad900e6531b58ff5c7e0b6bd642b41e40597
nzgamer41 <pslover9@gmail.com>
2019-04-01 10:36:53 +1300

    reset testing change

--- TeknoParrotUi/Properties/AssemblyInfo.cs
+++ TeknoParrotUi/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using TeknoParrotUi;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.9999")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
