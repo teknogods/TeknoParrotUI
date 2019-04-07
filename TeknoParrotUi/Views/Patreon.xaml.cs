81b95cf11c69cb53a6a2a3d3f5373b9552b33dff
nzgamer41 <pslover9@gmail.com>
2019-04-08 00:01:29 +1200

    fixed patreon

--- TeknoParrotUi/Views/Patreon.xaml.cs
+++ TeknoParrotUi/Views/Patreon.xaml.cs
@@ -34,7 +34,7 @@ namespace TeknoParrotUi.Views
                     var valueAsString = Encoding.ASCII.GetString(data); // GatewayServer
                     patreonKey.Text = valueAsString;
                     key.Close();
-                    _cmdStartInfo.FileName = "BudgieLoader.exe";
+                    _cmdStartInfo.FileName = ".\\TeknoParrot\\BudgieLoader.exe";
                     _cmdStartInfo.RedirectStandardOutput = true;
                     _cmdStartInfo.RedirectStandardInput = true;
                     _cmdStartInfo.UseShellExecute = false;
@@ -57,7 +57,7 @@ namespace TeknoParrotUi.Views
                 else
                 {
                     buttonDereg.Visibility = Visibility.Hidden;
-                    _cmdStartInfo.FileName = @"BudgieLoader.exe";
+                    _cmdStartInfo.FileName = ".\\TeknoParrot\\BudgieLoader.exe";
                     _cmdStartInfo.RedirectStandardOutput = true;
                     _cmdStartInfo.RedirectStandardError = true;
                     _cmdStartInfo.RedirectStandardInput = true;
