da6af39530c0ff621ae4f4185974ca676da6d6f4
nibs <kitten@riseup.net>
2020-02-26 00:50:01 -0500

    change button ordering

--- TeknoParrotUi.Common/Pipes/SpiceTools.cs
+++ TeknoParrotUi.Common/Pipes/SpiceTools.cs
@@ -12,21 +12,17 @@ namespace TeknoParrotUi.Common.Pipes
     {
         public override void Transmit()
         {
-            // Test
-            if (InputCode.PlayerDigitalButtons[0].Test.HasValue && InputCode.PlayerDigitalButtons[0].Test.Value)
-                Control |= 0x0001;
             // Service
             if (InputCode.PlayerDigitalButtons[0].Service.HasValue && InputCode.PlayerDigitalButtons[0].Service.Value)
-                Control |= 0x0002;
+                Control |= 0x01;
+            // Test
+            if (InputCode.PlayerDigitalButtons[0].Test.HasValue && InputCode.PlayerDigitalButtons[0].Test.Value)
+                Control |= 0x02;
             // Start
             if (InputCode.PlayerDigitalButtons[0].Start.HasValue && InputCode.PlayerDigitalButtons[0].Start.Value)
-                Control |= 0x0004;
+                Control |= 0x04;
 
             JvsHelper.StateView.Write(8, Control);
-            //JvsHelper.StateView.Write(12, InputCode.AnalogBytes[0]);
-
-            //JvsHelper.StateView.Write(16, InputCode.AnalogBytes[2]);
-            //JvsHelper.StateView.Write(20, InputCode.AnalogBytes[4]);
         }
     }
 }
