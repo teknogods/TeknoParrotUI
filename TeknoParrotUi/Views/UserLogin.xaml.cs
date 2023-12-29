7ca25f83294d7501bec165cdd152e63b1e9a5b01
Reaver <c0de187nig@gmail.com>
2023-12-29 02:28:25 +0200

    fix right click

--- TeknoParrotUi/Views/UserLogin.xaml.cs
+++ TeknoParrotUi/Views/UserLogin.xaml.cs
@@ -24,6 +24,31 @@ namespace TeknoParrotUi.Views
             //Browser.RegisterAsyncJsObject("callbackObj", _tPO2Callback);
             Browser.JavascriptObjectRepository.Settings.LegacyBindingEnabled = true;
             Browser.JavascriptObjectRepository.Register("callbackObj", _tPO2Callback, isAsync: true);
+            Browser.MenuHandler = new CustomMenuHandler();
+		}
+
+        public class CustomMenuHandler : CefSharp.IContextMenuHandler
+        {
+	        public void OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+	        {
+		        model.Clear();
+	        }
+
+	        public bool OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+	        {
+
+		        return false;
+	        }
+
+	        public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+	        {
+
+	        }
+
+	        public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+	        {
+		        return false;
+	        }
         }
 
         public void RefreshBrowserToStart()
