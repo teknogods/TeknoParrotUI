717c71ef066ecf4bd208e7b95d5640a0884763a0
Mike Waye <mpzmail1@gmail.com>
2024-05-07 01:15:44 +0100

    Revert "FNFSC Changes"
    This reverts commit 02ca0e0ef48bf24ed3bd34c1143c46d7f70a75a1.

--- TeknoParrotUi.Common/InputProfiles/Helpers/DigitalHelper.cs
+++ TeknoParrotUi.Common/InputProfiles/Helpers/DigitalHelper.cs
@@ -113,14 +113,6 @@ namespace TeknoParrotUi.Common.InputProfiles.Helpers
         {
             switch (gear)
             {
-                case 1:
-                    {
-                        InputCode.PlayerDigitalButtons[2].Button1 = true;
-                        InputCode.PlayerDigitalButtons[2].Button2 = false;
-                        InputCode.PlayerDigitalButtons[2].Button3 = false;
-                        InputCode.PlayerDigitalButtons[2].Button4 = false;
-                        CurrentFnfGear = 2;
-                    }
                 case 2:
                     {
                         InputCode.PlayerDigitalButtons[2].Button1 = false;
@@ -150,11 +142,11 @@ namespace TeknoParrotUi.Common.InputProfiles.Helpers
                     break;
                 default:
                     {
-                        InputCode.PlayerDigitalButtons[2].Button1 = false;
+                        InputCode.PlayerDigitalButtons[2].Button1 = true;
                         InputCode.PlayerDigitalButtons[2].Button2 = false;
                         InputCode.PlayerDigitalButtons[2].Button3 = false;
                         InputCode.PlayerDigitalButtons[2].Button4 = false;
-                        CurrentFnfGear = 0;
+                        CurrentFnfGear = 1;
                     }
                     break;
             }
