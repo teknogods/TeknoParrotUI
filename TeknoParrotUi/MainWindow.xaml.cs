639e32e1767750fbd428f3f01411ede816a85b12
nzgamer41 <pslover9@gmail.com>
2025-04-05 15:04:01 +1300

    Fix typo in updater

--- TeknoParrotUi/MainWindow.xaml.cs
+++ TeknoParrotUi/MainWindow.xaml.cs
@@ -331,92 +331,92 @@ namespace TeknoParrotUi
 
         public static List<UpdaterComponent> components = new List<UpdaterComponent>()
         {
-            new UpdaterComponent
-            {
-                name = "TeknoParrotUI",
-                location = Assembly.GetExecutingAssembly().Location
-            },
-            new UpdaterComponent
-            {
-                name = "OpenParrotWin32",
-                location = Path.Combine("OpenParrotWin32", "OpenParrot.dll"),
-                reponame = "OpenParrot"
-            },
-            new UpdaterComponent
-            {
-                name = "OpenParrotx64",
-                location = Path.Combine("OpenParrotx64", "OpenParrot64.dll"),
-                reponame = "OpenParrot"
-            },
-            new UpdaterComponent
-            {
-                name = "OpenSegaAPI",
-                location = Path.Combine("TeknoParrot", "Opensegaapi.dll"),
-                folderOverride = "TeknoParrot"
-            },
-            new UpdaterComponent
-            {
-                name = "TeknoParrot",
-                location = Path.Combine("TeknoParrot", "TeknoParrot.dll"),
-                opensource = false
-            },
-            new UpdaterComponent
-            {
-                name = "TeknoParrotN2",
-                location = Path.Combine("N2", "TeknoParrot.dll"),
-                reponame = "TeknoParrot",
-                opensource = false,
-                folderOverride = "N2"
-            },
-            /*
-            new UpdaterComponent
-            {
-                name = "SegaTools",
-                location = Path.Combine("SegaTools", "idzhook.dll"),
-                reponame = "SegaToolsTP",
-                folderOverride = "SegaTools",
-                userName = "nzgamer41"
-            },
-            */
-            new UpdaterComponent
-            {
-                name = "OpenSndGaelco",
-                location = Path.Combine("TeknoParrot", "OpenSndGaelco.dll"),
-                folderOverride = "TeknoParrot"
-            },
-            new UpdaterComponent
-            {
-                name = "OpenSndVoyager",
-                location = Path.Combine("TeknoParrot", "OpenSndVoyager.dll"),
-                folderOverride = "TeknoParrot"
-            },
-            new UpdaterComponent
-            {
-                name = "ScoreSubmission",
-                location = Path.Combine("TeknoParrot", "ScoreSubmission.dll"),
-                folderOverride = "TeknoParrot",
-                reponame = "TeknoParrot"
-            },
-            new UpdaterComponent
-            {
-                name = "TeknoDraw",
-                location = Path.Combine("TeknoParrot", "TeknoDraw64.dll"),
-                folderOverride = "TeknoParrot",
-                reponame = "TeknoParrot"
-            },
-            new UpdaterComponent
-            {
-                name = "TeknoParrotElfLdr2",
-                location = Path.Combine("ElfLdr2", "TeknoParrot.dll"),
-                reponame = "TeknoParrot",
-                opensource = false,
-                manualVersion = true,
-                folderOverride = "ElfLdr2"
-            },
+            //new UpdaterComponent
+            //{
+            //    name = "TeknoParrotUI",
+            //    location = Assembly.GetExecutingAssembly().Location
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "OpenParrotWin32",
+            //    location = Path.Combine("OpenParrotWin32", "OpenParrot.dll"),
+            //    reponame = "OpenParrot"
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "OpenParrotx64",
+            //    location = Path.Combine("OpenParrotx64", "OpenParrot64.dll"),
+            //    reponame = "OpenParrot"
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "OpenSegaAPI",
+            //    location = Path.Combine("TeknoParrot", "Opensegaapi.dll"),
+            //    folderOverride = "TeknoParrot"
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "TeknoParrot",
+            //    location = Path.Combine("TeknoParrot", "TeknoParrot.dll"),
+            //    opensource = false
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "TeknoParrotN2",
+            //    location = Path.Combine("N2", "TeknoParrot.dll"),
+            //    reponame = "TeknoParrot",
+            //    opensource = false,
+            //    folderOverride = "N2"
+            //},
+            ///*
+            //new UpdaterComponent
+            //{
+            //    name = "SegaTools",
+            //    location = Path.Combine("SegaTools", "idzhook.dll"),
+            //    reponame = "SegaToolsTP",
+            //    folderOverride = "SegaTools",
+            //    userName = "nzgamer41"
+            //},
+            //*/
+            //new UpdaterComponent
+            //{
+            //    name = "OpenSndGaelco",
+            //    location = Path.Combine("TeknoParrot", "OpenSndGaelco.dll"),
+            //    folderOverride = "TeknoParrot"
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "OpenSndVoyager",
+            //    location = Path.Combine("TeknoParrot", "OpenSndVoyager.dll"),
+            //    folderOverride = "TeknoParrot"
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "ScoreSubmission",
+            //    location = Path.Combine("TeknoParrot", "ScoreSubmission.dll"),
+            //    folderOverride = "TeknoParrot",
+            //    reponame = "TeknoParrot"
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "TeknoDraw",
+            //    location = Path.Combine("TeknoParrot", "TeknoDraw64.dll"),
+            //    folderOverride = "TeknoParrot",
+            //    reponame = "TeknoParrot"
+            //},
+            //new UpdaterComponent
+            //{
+            //    name = "TeknoParrotElfLdr2",
+            //    location = Path.Combine("ElfLdr2", "TeknoParrot.dll"),
+            //    reponame = "TeknoParrot",
+            //    opensource = false,
+            //    manualVersion = true,
+            //    folderOverride = "ElfLdr2"
+            //},
             new UpdaterComponent
             {
                 name = "FFBBlaster",
-                location = Path.Combine("FFBBlaster","x64","TeknoParrot.dll"),
+                location = Path.Combine("FFBBlaster","x64","FFBBlaster64.dll"),
                 reponame = "TeknoParrot",
                 opensource = false,
                 manualVersion = true,
