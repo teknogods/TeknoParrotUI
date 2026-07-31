42c900c325a9a509172cf49cc585523746d905a4
ReaverTeknoGods <c0de187nig@gmail.com>
2026-07-31 22:59:26 +0400

    Report Windows in legacy game analytics
    Keep the existing anonymous launch protocol and qualification behavior while adding an explicit Windows operatingSystem value and URL-encoding game names. Older servers remain compatible because the endpoint and existing parameters are unchanged.

--- TeknoParrotUi/Analytics.cs
+++ TeknoParrotUi/Analytics.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TeknoParrotUi.Common;
@@ -39,7 +35,9 @@ namespace TeknoParrotUi
                 _isRunning = true;
                 var myGuid =
                     await HttpGet(
-                            $"https://teknoparrot.com/Home/SimpleAnonData?emulatorModule={(int)emulationId}&gameName={gameName}")
+                            $"https://teknoparrot.com/Home/SimpleAnonData?emulatorModule={(int)emulationId}" +
+                            $"&gameName={Uri.EscapeDataString(gameName)}" +
+                            "&operatingSystem=Windows")
                         .ConfigureAwait(true);
                 for (int i = 0; i < 300; i++)
                 {
