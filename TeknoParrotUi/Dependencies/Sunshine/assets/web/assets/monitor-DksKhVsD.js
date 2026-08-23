9b1968cd5866af7d935a8dc111b8038a828e5156
leependegraft <82769910+Leemend907@users.noreply.github.com>
2026-08-22 21:27:23 -0500

    added portable sunshine files into project - can do differently later if wanted.

---
+++ TeknoParrotUi/Dependencies/Sunshine/assets/web/assets/monitor-DksKhVsD.js
@@ -0,0 +1,6 @@
+import{c as e}from"./createLucideIcon-CghN4yAT.js";/**
+ * @license lucide-vue-next v1.0.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=e("monitor",[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]]);export{o as M};
---
+++ TeknoParrotUi/Dependencies/Sunshine/assets/web/assets/rotate-ccw-BbbiZ6Cs.js
@@ -0,0 +1,6 @@
+import{c as t}from"./createLucideIcon-Dqb7x8z_.js";/**
+ * @license lucide-vue-next v1.0.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=t("rotate-ccw",[["path",{d:"M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"1357e3"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}]]);export{e as R};
